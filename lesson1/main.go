00c290fc0c413900bf3a7b937b3b2d383e11af17
slon <veremchukvv@gmail.com>
2020-02-17 23:53:22 +0300

    почистил закомменченый код

--- lesson1/main.go
+++ lesson1/main.go
@@ -17,9 +17,7 @@ func main() {
 
 func search(searchString string, searchPages []string) (foundings []string, err error) {
 
-	// var getBodies = make([]string, len(searchPages))
 	var getBodies = make(map[string]string)
-	// getBodies = make([]string, len(searchPages))
 
 	for i := range searchPages {
 		resp, err := http.Get(searchPages[i])
@@ -28,27 +26,18 @@ func search(searchString string, searchPages []string) (foundings []string, err 
 		}
 		defer resp.Body.Close()
 
-		// log.Printf("Status: %v;\n StatusCode: %v;\n Header: %v\n", resp.Status, resp.StatusCode, resp.Header)
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			return nil, err
 		}
 
 		BodyString := string(bodyBytes)
-		// getBodies = append(getBodies, searchPages[i], BodyString)
 		getBodies[searchPages[i]] = BodyString
-		// for key := range getBodies {
-		// 	fmt.Println(key)
-		// }
-
 	}
 
-	// log.Println(getBodies)
 	for key, value := range getBodies {
-		// log.Println(value)
 		if strings.Contains(value, searchString) {
 			foundings = append(foundings, key)
-			// log.Println("+")
 		}
 	}
 	log.Println(foundings)
