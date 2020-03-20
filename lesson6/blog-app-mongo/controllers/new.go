a1f1ee80864e4c7b9c620be84a787f0ecbe76f60
slon <veremchukvv@gmail.com>
2020-03-20 14:48:15 +0300

    разделил логику обработки хэндлера и запроса в базу в ещё одном контроллере

--- lesson6/blog-app-mongo/controllers/new.go
+++ lesson6/blog-app-mongo/controllers/new.go
@@ -14,18 +14,23 @@ type NewController struct {
 
 func (post *NewController) Get() {
 	post.TplName = "new.tpl"
-}
-
-func (post *NewController) Post() {
 	title := post.GetString("title")
 	author := post.GetString("author")
 	category := post.GetString("category")
 	text := post.GetString("text")
-	update := bson.D{{Key: "Title", Value: title}, {Key: "Author", Value: author}, {Key: "Category", Value: category}, {Key: "Text", Value: text}}
-	c := post.Explorer.Db.Database(post.Explorer.DbName).Collection("posts")
-	_, err := c.InsertOne(context.Background(), update)
+	err := post.Explorer.saveNewPost(title, author, category, text)
 	if err != nil {
 		log.Println(err)
 	}
 	post.Redirect("/", 302)
+}
+
+func (e Explorer) saveNewPost(title, author, category, text string) error {
+	update := bson.D{{Key: "Title", Value: title}, {Key: "Author", Value: author}, {Key: "Category", Value: category}, {Key: "Text", Value: text}}
+	c := e.Db.Database(e.DbName).Collection("posts")
+	_, err := c.InsertOne(context.Background(), update)
+	if err != nil {
+		log.Println(err)
+	}
+	return nil
 }
